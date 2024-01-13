176f760e37e5d8340d72b0e47e6f05793597c4e3
xBlackEaglex <roberto.smusic.1@gmail.com>
2024-01-12 23:34:27 -0600

    se modifico el navbar para que fuera permanente y un poco transparente

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,27 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'rosa': { 
+				'50': '#fbf4f7', 
+				'100': '#f8ebf0', 
+				'200': '#f2d8e3', 
+				'300': '#e7b5c9', 
+				'400': '#d98daa', 
+				'500': '#ca6a8b', 
+				'600': '#b64c6c', 
+				'700': '#9c3a55',
+				'800': '#813347', 
+				'900': '#6d2e3e', 
+				'950': '#411621',
+			},
+			'azul': '#011957',
+			'white': '#FFFFFF',
+			'black': '#000000',
+			transparent: 'transparent',
+			current: 'currentColor',
+			
+		}
 	},
 	plugins: [],
 }
